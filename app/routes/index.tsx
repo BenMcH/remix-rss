bed4e2b0e2bed132257b012506125e585b5235ab
Ben McHone <benjamin.mchone@gmail.com>
2021-05-12 21:16:49 -0500

    Added searching and dynamic views

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,5 @@
 import Parser from 'rss-parser';
-import type { MetaFunction, LinksFunction, LoaderFunction } from "remix";
+import { MetaFunction, LinksFunction, LoaderFunction, Form, Link } from "remix";
 import { useRouteData } from "remix";
 
 import stylesUrl from "../styles/index.css";
@@ -16,34 +16,52 @@ export let links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export let loader: LoaderFunction = async () => {
-  const feed = await getFeed('https://www.engadget.com/rss.xml');
+export let loader: LoaderFunction = async ({request}) => {
+  const params: any = {}
+  const {searchParams} = new URL(request.url);
+  const feedParam = searchParams.get('feed');
+  if (feedParam) {
+    const feed = await getFeed(feedParam);
 
-  return {feed};
+    return {feed};
+  }
+
+  return {}
 };
 
 export default function Index() {
   let data = useRouteData();
 
-  const feed = data.feed as Parser.Output<{ [key: string]: any; }>;
+  if (data.feed) {
+    const feed = data.feed as Parser.Output<{ [key: string]: any; }>;
 
-  console.log({feed: data.feed});
+    return (
+      <div style={{maxWidth: '600px', margin: '0 auto'}}>
+        <Link to="/">{'< Return Home'}</Link>
+        <h2>{feed.title}</h2>
+        <h4>{feed.description}</h4>
+        <img src={feed.image?.url} />
+
+        <ul>
+        {feed.items.map(item => (
+          <li key={item.isoDate}>
+            <p>{item.title}</p>
+            <p>{item.contentSnippet}</p>
+            <p><a href={item.link} target="_blank">{'Read More'}</a></p>
+          </li>
+        ))}
+        </ul>
+      </div>
+    );
+  }
 
   return (
-    <div style={{maxWidth: '600px', margin: '0 auto'}}>
-      <h2>{feed.title}</h2>
-      <h4>{feed.description}</h4>
-      <img src={feed.image?.url} />
-
-      <ul>
-      {feed.items.map(item => (
-        <li key={item.isoDate}>
-          <p>{item.title}</p>
-          <p>{item.contentSnippet}</p>
-          <p><a href={item.link} target="_blank">{'Read More'}</a></p>
-        </li>
-      ))}
-      </ul>
-    </div>
-  );
+    <main>
+      <h1>Welcome to the RSS Reader</h1>
+      <Form method="get">
+        <label>{'RSS Feed:'} <input type="text" name="feed"/></label>
+        <button type="submit">{'Go'}</button>
+      </Form>
+    </main>
+  )
 }
