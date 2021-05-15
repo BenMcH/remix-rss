ff979171265f5d1783d7ac3f784cd095f9b39c19
Ben McHone <benjamin.mchone@gmail.com>
2021-05-14 21:43:34 -0500

    Added GTM

--- app/root.tsx
+++ app/root.tsx
@@ -21,8 +21,16 @@ function Document({ children }: { children: React.ReactNode }) {
         <link rel="icon" href="/favicon.png" type="image/png" />
         <Meta />
         <Links />
+        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KWPRT5Q');`} }/>
       </head>
       <body>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWPRT5Q" height="0" width="0" style={{display: 'none', visibility:'hidden'}}></iframe>
+        </noscript>
         {children}
 
         <Scripts />
