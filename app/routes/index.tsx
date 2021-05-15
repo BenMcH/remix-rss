56c89625118c0749972dbfc06b43caa1b000250a
Ben McHone <benjamin.mchone@gmail.com>
2021-05-14 21:04:25 -0500

    Made read more button more permissive

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -69,7 +69,7 @@ const FeedItem: React.FC<{item: Parser.Item}> = ({item}) => {
     <li key={item.isoDate}>
       <h3>{item.title}</h3>
       <p>
-        {contentSnippet !== item.contentSnippet && (
+        {contentSnippet !== item.content && (
           <>
             <a href="#" onClick={(e) => {e.preventDefault(); setShowAllContent(!showAllContent)}}>{showAllContent ? 'Read Less' : 'Read More'}</a>
             {' | '}
