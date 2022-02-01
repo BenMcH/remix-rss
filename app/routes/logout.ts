9ab643b0ecf86bc209697c81368b529cf39a9148
Ben McHone <benjamin.mchone@gmail.com>
2022-02-01 08:09:15 -0600

    URLSearchParams on logout

--- app/routes/logout.ts
+++ app/routes/logout.ts
@@ -2,5 +2,9 @@ import { LoaderFunction } from 'remix';
 import { authenticator, getHost } from '~/services/auth.server';
 
 export let loader: LoaderFunction = async ({request}) => {
-	await authenticator.logout(request, { redirectTo: `https://mchone.us.auth0.com/v2/logout?client_id=${process.env.CLIENT_ID}&returnTo=${getHost()}` });
+	let params = new URLSearchParams();
+	params.append('returnTo', getHost());
+	params.append('client_id', process.env.CLIENT_ID!);
+
+	await authenticator.logout(request, { redirectTo: `https://mchone.us.auth0.com/v2/logout?${params.toString()}` });
 }
