dd3f582acaec533594ec3aa559eb313d40f07c82
Ben McHone <benjamin.mchone@gmail.com>
2022-02-03 20:23:36 -0600

    Better design

--- app/components/FeedItem.tsx
+++ app/components/FeedItem.tsx
@@ -15,34 +15,26 @@ const FeedItem: React.FC<{item: FeedItemPost}> = ({item}) => {
 
   const [open, setOpen] = useState(false);
 
-  const [hasOpened, setHasOpened] = useState(false)
-
-  useEffect(() => {
-    if (open) {
-      setHasOpened(true)
-    }
-  }, [open, setHasOpened])
-
   const renderedDate = useMemo(() => {
     const date = new Date(item.date);
-    return date.toLocaleString([], {day: '2-digit', month: '2-digit', year: '2-digit', hour: '2-digit', minute:'2-digit'}); 
+    return date.toLocaleString([], {day: '2-digit', month: '2-digit', year: '2-digit'}); 
   }, [item.date]);
 
   return (
     <>
-      <tr className={hasOpened ? "" : "border-l"} onClick={() => setOpen(!open)}>
-        <td className="md:truncate md:max-w-screen-2xl"><span className="text-sm font-bold">{item.title}</span> {!open && <span className="text-xs font-light hidden md:inline">{contentSnippet}</span>}</td>
-        <td className="text-xs pl-4">{renderedDate}</td>
+      <tr className="cursor-pointer border-t" onClick={() => setOpen(!open)}>
+        <td className={`pl-2 ${open ? 'pt-2' : 'py-2'} md:truncate md:max-w-screen-2xl`}><span className="text-sm font-bold">{item.title}</span> {!open && <span className="text-xs font-light hidden md:inline">{contentSnippet}</span>}</td>
+        <td className="py-2 text-xs pl-4 text-right">{renderedDate}</td>
       </tr>
       {open &&(
         <>
           <tr>
-            <td colSpan={2} className="pl-4 border-l border-l-slate-500">
-              {open ?  <p dangerouslySetInnerHTML={{__html: item.content || ''}} /> : <>{contentSnippet}</>}
+            <td colSpan={2} className="pl-2 border-l border-l-slate-500">
+              <p dangerouslySetInnerHTML={{__html: item.content || ''}} />
             </td>
           </tr>
           <tr>
-            <td className="pl-4" colSpan={2}>
+            <td className="pl-4 pb-2" colSpan={2}>
               <a href={item.link} target='_blank'>{'Open Link'}</a>
             </td>
           </tr>
