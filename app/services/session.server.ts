8a34c4789234043117b681bac50ad067b09adb19
Ben McHone <benjamin.mchone@gmail.com>
2022-02-03 19:23:01 -0600

    Configure actual session secret

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -4,14 +4,18 @@ import { createCookieSessionStorage } from 'remix';
 // export the whole sessionStorage object
 export let sessionStorage = createCookieSessionStorage({
   cookie: {
-    name: '_session', // use any name you want here
-    sameSite: 'lax', // this helps with CSRF
-    path: '/', // remember to add this so the cookie will work in all routes
-    httpOnly: true, // for security reasons, make this cookie http only
-    secrets: ['s3cr3t'], // replace this with an actual secret
-    secure: process.env.NODE_ENV === 'production', // enable this in prod only
+    name: '_session',
+    sameSite: 'lax',
+    path: '/',
+    httpOnly: true,
+    secrets: [process.env.SESSION_SECRET || 'secret'],
+    secure: process.env.NODE_ENV === 'production',
   },
 });
+
+if (process.env.NODE_ENV === 'production' && !process.env.SESSION_SECRET) {
+  throw new Error('Missing SESSION_SECRET env');
+}
 
 // you can also export the methods individually for your own usage
 export let { getSession, commitSession, destroySession } = sessionStorage;
