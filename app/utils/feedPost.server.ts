557f8ad65745b0411dbc0618dd0f6d538184c135
Ben McHone <benjamin.mchone@gmail.com>
2022-02-06 19:25:44 -0600

    Create many on feedpost instead of manual looping

--- app/utils/feedPost.server.ts
+++ app/utils/feedPost.server.ts
@@ -16,25 +16,15 @@ export async function insertFeedPosts(feed: InternalFeed) {
     return;
   }
 
-  await Promise.all(
-    feed.items.map((post) => 
-      db.feedPost.upsert({
-        where: {
-          feedId_date_link: {
-            date: post.date,
-            feedId: dbFeed.id,
-            link: post.link,
-          }
-        },
-        create: {
-            feedId: dbFeed.id,
-            title: post.title,
-            link: post.link,
-            date: post.date,
-            content: post.content,
-            contentSnippet: post.contentSnippet,
-        },
-        update: {}
-      })
-  ));
+  await db.feedPost.createMany({
+	  data: feed.items.map((post) => ({
+		  content: post.content,
+		  contentSnippet: post.contentSnippet,
+		  date: post.date,
+		  feedId: dbFeed.id,
+		  link: post.link,
+		  title: post.title
+	  })),
+	  skipDuplicates: true
+  });
 }
