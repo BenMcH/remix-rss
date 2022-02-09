06c71a37a2c58856955e9d4c5fb17d5885727ce4
Ben McHone <benjamin.mchone@gmail.com>
2022-02-08 18:13:33 -0600

    Made feeds noscript friendly

--- app/components/FeedItem.tsx
+++ app/components/FeedItem.tsx
@@ -20,7 +20,13 @@ const FeedItem: React.FC<{item: FeedItemPost}> = ({item}) => {
   return (
     <>
       <tr className="cursor-pointer border-t" onClick={() => setOpen(!open)} onPointerEnter={() => fetcher.state === 'idle' && !fetcher.data && fetcher.load(`/api/posts/${item.id}`)}>
-        <td className={`${open ? 'pt-2' : 'py-2'} md:truncate md:max-w-screen-2xl`}><span className="pl-2 text-sm font-bold">{item.title}</span> {!open && <span className="text-xs font-light hidden md:inline">{contentSnippet}</span>}</td>
+        <td className={`${open ? 'pt-2' : 'py-2'} md:truncate md:max-w-screen-2xl`}>
+          <span className="pl-2 text-sm font-bold">{item.title}</span>
+          {!open && <span className="text-xs font-light hidden md:inline">{contentSnippet}</span>}
+          <noscript>
+            <a href={item.link} target='_blank'>{'Open Link'}</a>
+          </noscript>
+        </td>
         <td className={`${open ? 'pt-2' : 'py-2'} text-xs pl-4 text-right`}>{renderedDate}</td>
       </tr>
       {open &&(
