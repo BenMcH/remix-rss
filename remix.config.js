d2b2b69af50a62f5b6428a8a7a981a0010da035a
Ben McHone <benjamin.mchone@gmail.com>
2022-02-16 19:34:45 -0600

    Updated remix

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,7 @@ module.exports = {
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildDirectory: "build",
+  serverBuildTarget: "node-cjs",
   devServerPort: 8002,
   ignoredRouteFiles: [".*"]
 };
